73978a52c6c9e7aff39db9dd669cf5ae40918c41
eduregis <eduregis1@gmail.com>
2022-09-21 15:01:06 -0300

    change some definitions in scriptableObjects

--- Assets/Scripts/ScriptableObjects/BasicCharacterStats.cs
+++ Assets/Scripts/ScriptableObjects/BasicCharacterStats.cs
@@ -8,13 +8,26 @@ public class BasicCharacterStats : ScriptableObject
 {
     public string char_name;
 
+    [Header("Characters Basic Stats")]
     public int strength;
     public int intelligence;
     public int vitality;
     public int technique;
     public int agility;
     public int luck;
-    public int level;
+
+    [Header("Characters Battle Stats")]
+    public int life;
+    public int maxLife;
+    public int energy;
+    public int maxEnergy;
+    public float defense;
+    public int damage;
+    public int hitRate;
+    public int evasionRate;
+    public int critRate;
+    public int critDamage;
+    public bool isBlocking;
 
     public List<CharacterSkill> skills;
 }
