946833a1b2b3587b998dad522d47877e942972ad
eduregis <eduregis1@gmail.com>
2022-10-28 22:11:16 -0300

    encrypt and decrypt data

--- Assets/Scripts/DataPersistence/FileDataHandler.cs
+++ Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -7,10 +7,13 @@ using System.IO;
 public class FileDataHandler {
     private string dataDirPath = "";
     private string dataFileName = "";
+    private bool useEncryption = false;
+    private readonly string encryptionCodeWord = "word";
 
-    public FileDataHandler(string dataDirPath, string dataFileName) {
+    public FileDataHandler(string dataDirPath, string dataFileName, bool useEncryption) {
         this.dataDirPath = dataDirPath;
         this.dataFileName = dataFileName;
+        this.useEncryption = useEncryption;
     }
 
     public GameData Load() {
@@ -19,12 +22,17 @@ public class FileDataHandler {
         if (File.Exists(fullPath)) {
             try {
                 string dataToLoad = "";
-                using(FileStream stream = new FileStream(fullPath, FileMode.Open)) {
+                using (FileStream stream = new FileStream(fullPath, FileMode.Open)) {
                     using (StreamReader reader = new StreamReader(stream)) {
                         dataToLoad = reader.ReadToEnd();
                     }
                 }
+                // optionally decrypt the data
+                if (useEncryption) {
+                    dataToLoad = EncryptDecrypt(dataToLoad);
+                }
                 loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+                Debug.Log(fullPath);
             }
             catch (Exception e) {
                 Debug.LogError("Error occured when trying to load data from file: " + fullPath + "\n" + e);
@@ -34,10 +42,18 @@ public class FileDataHandler {
     }
 
     public void Save(GameData data) {
+        // use Path.Combine to account for different OS's having different path separators
         string fullPath = Path.Combine(dataDirPath, dataFileName);
         try {
+            // create the diretory the file will be written to if it doesn't already exist
             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+            // serialize the C# game data object into JSON
             string dataToStore = JsonUtility.ToJson(data, true);
+            // optionally encrypt the data
+            if (useEncryption) {
+                dataToStore = EncryptDecrypt(dataToStore);
+            }
+            // write the serialized data to the file
             using (FileStream stream = new FileStream(fullPath, FileMode.Create)) {
                 using (StreamWriter writer = new StreamWriter(stream)) {
                     writer.Write(dataToStore);
@@ -46,5 +62,14 @@ public class FileDataHandler {
         } catch (Exception e) {
             Debug.LogError("Error occured when trying to save data to file: " + fullPath + "\n" + e);
         }
+    }
+
+    // the below is a simple implementation of XOR encryption
+    private string EncryptDecrypt(string data) {
+        string modifiedData = "";
+        for (int i = 0; i < data.Length; i++) {
+            modifiedData += (char)(data[i] ^ encryptionCodeWord[i % encryptionCodeWord.Length]);
+        }
+        return modifiedData;
     }
 }
